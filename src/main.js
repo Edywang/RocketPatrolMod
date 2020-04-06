1e9196740ed2f4415791b418b9584dd295fc81c4
Edywang <edwinwang00@gmail.com>
2020-04-06 15:29:44 -0700

    Initial commit: file structure

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Terminal command to start localhost web server: "python -m http.server"
+console.log("JS File Loaded Successfully");
+let config = {
+    // Render type: OpenGL or Canvas; Choosing Canvas for our type of art
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480
+};
+// Starting the Phaser game with the object config as a param
+let game = new Phaser.Game(config);
